92e8f7c1eb73d02157c8cd8de2a61c441ccdfd3b
Anonymous <>
2019-01-11 01:25:01 -0500

    fix logo

--- src/controllers/sling.js
+++ src/controllers/sling.js
@@ -5,6 +5,7 @@ const m3u8 = require('m3u8');
 const url = require('url');
 const uuidv4 = require('uuid/v4');
 const XmlJs = require('xml2js');
+const sharp = require('sharp');
 
 const XmlParser = new XmlJs.Parser();
 const widevineUrl = 'http://p-drmwv.movetv.com/proxy';
@@ -296,10 +297,15 @@ class Sling extends Controller {
       }
       return this.getRawSchedule(ctx).then(res => {
         const channel = res.channels.find(x => x.channel_name === channelName);
-        if (channel && channel.image && channel.image.url) {
-          return rp.get({ url: channel.image.url, encoding: null }).then(img => {
-            ctx.cache.setex(cacheId, 86400, img.toString('base64'));
-            return img;
+        if (channel && channel.thumbnail_cropped && channel.thumbnail_cropped.url) {
+          return rp.get({ url: channel.thumbnail_cropped.url, encoding: null }).then(img => {
+            return sharp(img)
+              .resize(150)
+              .toBuffer()
+              .then(data => {
+                ctx.cache.setex(cacheId, 86400, data.toString('base64'));
+                return data;
+              });
           });
         }
       });
